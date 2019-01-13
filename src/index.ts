4a6fca341afa58c2657f1b971702175fbf36a81d
Thomas McAuliffe <tjamesmac@gmail.com>
2019-01-13 09:55:06 +0000

    Add components to test typescript & styling

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,17 @@
-import './style.css';
+import './style.scss';
 
 const component = () => {
 
-    const element = document.createElement('div');
+    const element: HTMLElement = document.createElement('div');
     let elementText: string = "Welcome to TypeScript";
+    element.innerHTML = elementText;
+
+    return element;
+}
+const innerComponent = () => {
+
+    const element: HTMLElement = document.createElement('p');
+    let elementText: string = `I'm inside the div`;
     element.innerHTML = elementText;
 
     return element;
@@ -14,3 +22,5 @@ let test: string = "Hello";
 console.log(test);
 
 document.body.appendChild(component());
+const target = document.getElementsByTagName('div')[0];
+target.appendChild(innerComponent());
