c86665b2851afcf6d4b9d1d403434ac50d8b6f6f
Thomas McAuliffe <tjamesmac@gmail.com>
2019-01-14 13:14:26 +0000

    Add variables that are included in the interface (not sure is right)

--- src/weather.ts
+++ src/weather.ts
@@ -1,14 +1,39 @@
+
+// would like to build a weather class that accepts the data from the API
+
 export class Weather {
-    weather: string;
-    place: string;
-    constructor(forecast: string, location: string) {
-        this.weather = forecast;
-        this.place = location;
+    // weather: string;
+    // place: string;
+    clouds: object;
+    name: string;
+
+    // main
+    temperature: number;
+    tempMax: number;
+    tempMin: number;
+    precipitation: number;
+    humidity: number;
+
+    // sys
+    country: string;
+    sunrise: number;
+    sunset: number;
+
+    // weather
+    description: string;
+    icon: string;
+    id: number;
+    
+    // wind
+    wind: object
+    constructor(forecast: string, location: string, ) {
+        this.description = forecast;
+        this.name = location;
     }
     forecast() {
-        return this.weather;
+        return this.forecast;
     }
     location() {
-        return this.place;
+        return this.location;
     }
 }
