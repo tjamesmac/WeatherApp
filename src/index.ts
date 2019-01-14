81e537d2957104f2a4196b4812bc8fb5c5ee04d8
Thomas McAuliffe <tjamesmac@gmail.com>
2019-01-14 13:14:50 +0000

    Add interface and innerComponent

--- src/index.ts
+++ src/index.ts
@@ -3,53 +3,71 @@ import { weatherCall } from './api';
 import { Weather } from './weather';
 
 const API_KEY: string = '2c0fd3dc783f393963e1175852aa9392';
-const baseURL: string = 'https://api.openweathermap.org/data/2.5/weather?q=Blackwood,uk&appid=';
+const baseURL: string = 'https://api.openweathermap.org/data/2.5/weather?units=metric&q=Blackwood,uk&appid=';
 const apiURL: string = baseURL + API_KEY;
 
 let data = weatherCall(apiURL);
 
+interface WeatherForecast {
+
+    clouds: object
+    name: string,
+
+    // main
+    temperature: number,
+    tempMax: number,
+    tempMin: number,
+    precipitation: number,
+    humidity: number,
+
+    // sys
+    country: string,
+    sunrise: number,
+    sunset: number,
+
+    // weather
+    description: string,
+    icon: string,
+    id: number,
+
+    // wind
+    wind: object
+
+}
+
 data.then(res => console.log(res));
 data.then(res => {
-    let weather = new Weather(res.weather[0].description, res.name);
+    // create new object here and then call weatherComponent build function
+    let weather: WeatherForecast = new Weather(res.weather[0].description, res.name);
     console.log(weather);
-    document.body.appendChild(weatherComponent(weather.forecast(), weather.location()));
+    document.body.appendChild(weatherComponent(weather));
 });
 
 /* I think I need to use an interface here */
 
-const weatherComponent = (forecast: string, location: string) => {
+// function will accept a weather object and then build accordingly
+const weatherComponent = (weather: WeatherForecast) => {
 
-    
+
     const element: HTMLElement = document.createElement('div');
-    let weatherDesc: string = forecast
-    let weatherLocation: string = location;
+    let weatherDesc: HTMLElement = innerComponent(weather.description);
+    let weatherLocation: HTMLElement = innerComponent(weather.name);
 
-    element.innerHTML = weatherLocation;
+    // element.innerHTML = weatherLocation;
+
+    element.appendChild(weatherDesc);
+    element.appendChild(weatherLocation);
 
     return element;
 }
-const component = () => {
-
-    const element: HTMLElement = document.createElement('div');
-    let elementText: string = "Welcome to TypeScript";
-
-    element.innerHTML = elementText;
-
-    return element;
-}
-const innerComponent = () => {
+// using to test results
+// Don't think this is the right way to do this, but if I continued to do so, make it loop so I don't have to call it 1,000,000 times
+const innerComponent = (property: any) => {
 
     const element: HTMLElement = document.createElement('p');
-    let elementText: string = `I'm inside the div`;
-    element.innerHTML = elementText;
+    let prop: any = property;
+    element.innerHTML = prop;
 
     return element;
+
 }
-
-let test: string = "Hello";
-
-console.log(test);
-
-document.body.appendChild(component());
-const target = document.getElementsByTagName('div')[0];
-target.appendChild(innerComponent());
