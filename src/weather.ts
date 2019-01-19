a702f3ec4fec6fe0f8586d260ed429ad4da8c1f4
Thomas McAuliffe <tjamesmac@gmail.com>
2019-01-19 16:22:38 +0000

    Replace the class with functions for DOM

--- src/weather.ts
+++ src/weather.ts
@@ -1,19 +1,58 @@
+export const weatherHeader = (weather: string, className: string): HTMLElement  => {
 
-// // would like to build a weather class that accepts the data from the API
+    let headData: HTMLDivElement = document.createElement('div');
+    headData.classList.add(className);
+    headData.innerHTML = weather;
 
-// interface WeatherData {
-//     name?: string;
-// }
+    return headData;
 
-// export class Weather {
+}
 
-//     name: string;
+export const weatherMiddle = (weather: string, className: string): HTMLElement  => {
 
-//     constructor(private data: any) {
+    let headData: HTMLDivElement = document.createElement('div');
+    headData.classList.add(className);
+    headData.innerHTML = weather;
 
-//     }
+    return headData;
 
-//     getName() {
-//         return this.data;
-//     }
-// }
+}
+
+export const weatherIcon = (icon: string, desc: string, className: string): HTMLElement => {
+
+    let headData: HTMLDivElement = document.createElement('div');
+    headData.classList.add(className);
+    headData.innerHTML = `<img src="http://openweathermap.org/img/w/${icon}.png"/>`
+
+    let description: HTMLElement = document.createElement('span');
+    description.innerHTML = desc;
+
+    headData.appendChild(description);
+
+    return headData;
+
+}
+
+
+export const weatherData = (main: any, className: string) => {
+
+    let weatherData: HTMLDivElement = document.createElement('div');
+
+    let max: number = main.temp_max;
+    let min: number = main.temp_min;
+    let pressure: number = main.pressure;
+    let humidity: number = main.humidity;
+
+    let dater: number[] = [max, min, pressure, humidity]
+
+    for (let element of dater) {
+
+        let weatherDataBox = document.createElement('span');
+        weatherDataBox.innerHTML = `${element}, `;
+        weatherData.appendChild(weatherDataBox);
+
+    }
+    
+    return weatherData;
+}
+
