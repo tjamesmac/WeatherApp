eab95092009bffaf8ceecf63e7318ab23958abb2
Thomas McAuliffe <tjamesmac@gmail.com>
2019-01-19 16:22:12 +0000

    Add functions that add data to the DOM

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import './style.scss';
+
 import { weatherCall } from './api';
-// import { Weather } from './weather';
+import { weatherHeader, weatherData } from './weather';
+import { weatherMiddle } from './weather';
+import { weatherIcon } from './weather';
 
 const API_KEY: string = '2c0fd3dc783f393963e1175852aa9392';
 const baseURL: string = 'https://api.openweathermap.org/data/2.5/weather?units=metric&q=Blackwood,uk&appid=';
@@ -24,43 +27,57 @@ interface responseData {
             description: string;
             id: number;
             icon: string;
+            main: string;
         }
     }
 }
 
 
-// working block with the interface WeatherData //
+// need to add error checking to the api call 
+
+// I want to bundle my dom elements together and build them at once
+
+// I want one function that accepts many smaller functions to compile the dom
+// elements together in one thing
+
+//  description
+//  numbers
+//  icons and stuff
+
 let data = weatherCall(apiURL);
 data.then(res => console.log(res));
 data.then(res => {
 
     // adding interface to my returned json object;
     let response: responseData = res;
+    console.log(response);
 
     let name: string = response.name;
     let country: string = response.sys.country;
     let nameAndCountry: string = `${name}, ${country}`;
-    
     let description: string = response.weather[0].description;
+    let icon: string = response.weather[0].icon;
+    let miniDesc: string = response.weather[0].main;
+    let main: object = response.main;
+    
 
-    let temperature: number = response.main.temp;
 
-    let overview: any[] = [nameAndCountry, description];
+    weatherComponent(weatherHeader(nameAndCountry, "title"));
+    weatherComponent(weatherHeader(description, "description"));
+    weatherComponent(weatherIcon(icon, miniDesc, "weatherIcon"));
+    weatherComponent(weatherData(main, "weatherIcon"));
 
-    for (let prop of overview) {
 
-        weatherComponent(prop);
 
-    }
+
 
 });
 
+// this can be my generic function to add things to the dom
 const weatherComponent = (weather: any) => {
 
-    const element = document.getElementsByClassName('container')[0];
-    console.log(element);
-    let elementData: HTMLElement = innerComponent(weather);
-     element.appendChild(elementData);
+    const element = document.getElementsByClassName('col')[0];
+    element.appendChild(weather);
 
 }
 
@@ -78,9 +95,15 @@ const innerComponent = (property: any) => {
 const app = () => {
 
     const body: HTMLDivElement = document.createElement('div');
-    body.classList.add("container");
+    const row: HTMLDivElement = document.createElement('div');
+    const col: HTMLDivElement = document.createElement('div');
 
+    body.classList.add("container");
+    row.classList.add('row');
+    col.classList.add('col');
     document.body.appendChild(body);
+    body.appendChild(row);
+    row.appendChild(col);
 
 }
 
