b5e00558de6f3e3871ae5daba4e5f46340e5c0b9
Thomas McAuliffe <tjamesmac@gmail.com>
2019-01-19 16:35:20 +0000

    Remove console.logs

--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,5 @@
 export async function weatherCall(apiURL: string) {
     let response = await fetch(apiURL);
-    console.log(response);
-    console.log(typeof response);
     if (response.status === 200) {
         let data = await response.json();
         return data;
